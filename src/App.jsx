4a56a3a5dda20fbb12ce53aff8627e2afde19d61
Tushar <tusharkumar0807@gmail.com>
2022-07-01 10:46:24 +0530

    header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
