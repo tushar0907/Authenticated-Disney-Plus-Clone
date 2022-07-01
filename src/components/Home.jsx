216480a0065082ede22dcc517c90b47ee7734a7a
Tushar <tusharkumar0807@gmail.com>
2022-07-01 17:18:36 +0530

    viewers section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
+import ImgSlider from "../components/ImgSlider";
+import Viewers from "../components/Viewers";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewers />
+    </Container>
+  );
 };
 
 export default Home;
+
+const Container = styled.main`
+min-height: calc(100vh - 70px)
+padding: 0 calc(3.5vw + 5px)
+position: relative;
+overflow-x: hidden;
+overflow-y: hidden;
+
+&:before{
+  background: url("/images/home-background.png") center center / cover;
+  no-repeat: fixed;
+  content:"";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom:0;
+  z-index: -1;
+}
+`;
